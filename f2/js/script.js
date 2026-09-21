9c80a0291a42f08e65d661841dee249fae2110be
CRBAGI <120530411+CRBAGI@users.noreply.github.com>
2026-09-21 08:25:14 -0600

    Update script.js

--- f2/js/script.js
+++ f2/js/script.js
@@ -4,16 +4,9 @@
 
 // ---------- PERSONALIZACIÓN (editar aquí sin tocar el resto) ----------
 const titulo = "Feliz día de las flores amarillas";
-const subtitulo = ":)";
 const nombrePersona = ""; // si se rellena, se añade al final del título
 
-const mensajesRomanticos = [
-  "Eres el color más bonito de mi día 🌻",
-  "Contigo hasta el negro se llena de luz",
-  "Un girasol para la persona que ilumina todo",
-  "Gracias por existir 💛",
-  "Este ramo creció solo para ti"
-];
+
 
 // ---------- Utilidades ----------
 const $ = (sel, ctx = document) => ctx.querySelector(sel);
